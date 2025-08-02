04a4c549a477c5ad31d7e347f335e98d4bbb5551
Mufth <jeppepedas@hotmail.com>
2025-08-02 16:04:18 +0200

    added a13

--- App.jsx
+++ App.jsx
@@ -1,11 +1,18 @@
-import { useState } from "react"
+import { useState, useRef, useEffect } from "react"
 import Die from "./Die"
 import { nanoid } from "nanoid"
 import Confetti from "react-confetti"
 
 export default function App() {
-    const [dice, setDice] = useState(generateAllNewDice())
+    const [dice, setDice] = useState(() => generateAllNewDice())
+    const btnRef = useRef(null)
     const isWinning = dice.every(die => die.isHeld && die.value === dice[0].value)
+
+    useEffect(() => {
+        if (isWinning) {
+            btnRef.current.focus()
+        }
+    }, [isWinning])
 
     function generateAllNewDice() {
         return new Array(10)
@@ -46,12 +53,12 @@ export default function App() {
         <main>
             {isWinning && <Confetti />}
             <h1 className="title">Tenzies</h1>
-            {isWinning ? <p className="winning">You won! Click to play again.</p> 
+            {isWinning ? <p aria-live="polite" className="winning">You won! Press "New Game" to play again.</p>
             : <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>}
             <div className="dice-container">
                 {diceElements}
             </div>
-            <button className="roll-dice" onClick={rollDice}>{isWinning ? "New game" : "Roll"}</button>
+            <button ref={btnRef} className="roll-dice" onClick={rollDice}>{isWinning ? "New game" : "Roll"}</button>
         </main>
     )
 }
--- Die.jsx
+++ Die.jsx
@@ -7,6 +7,10 @@ export default function Die(props) {
         <button 
             style={styles}
             onClick={props.hold}
+            className="die"
+            aria-pressed={props.isHeld}
+            aria-label={`Die showing ${props.value}, ${props.isHeld ?
+                 "held" : "not held"}`}
         >{props.value}</button>
     )
 }
