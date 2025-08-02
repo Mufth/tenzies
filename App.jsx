8feae9dedb00d2628b07e782651bb520471ca281
Mufth <jeppepedas@hotmail.com>
2025-08-02 11:13:58 +0200

    Added final selction logic, and endgame conditions and confetti

--- App.jsx
+++ App.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react"
 import Die from "./Die"
 import { nanoid } from "nanoid"
+import Confetti from "react-confetti"
 
 export default function App() {
     const [dice, setDice] = useState(generateAllNewDice())
+    const isWinning = dice.every(die => die.isHeld && die.value === dice[0].value)
 
     function generateAllNewDice() {
         return new Array(10)
@@ -16,7 +18,13 @@ export default function App() {
     }
 
     function rollDice() {
-        setDice(generateAllNewDice())
+        if (isWinning) {
+            setDice(generateAllNewDice())
+            return
+        }
+        setDice(oldDice => oldDice.map(die => (
+            die.isHeld ? die : { ...die, value: Math.ceil(Math.random() * 6) }
+        )))
     }
 
     function hold(id) {
@@ -36,10 +44,14 @@ export default function App() {
 
     return (
         <main>
+            {isWinning && <Confetti />}
+            <h1 className="title">Tenzies</h1>
+            {isWinning ? <p className="winning">You won! Click to play again.</p> 
+            : <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>}
             <div className="dice-container">
                 {diceElements}
             </div>
-            <button className="roll-dice" onClick={rollDice}>Roll</button>
+            <button className="roll-dice" onClick={rollDice}>{isWinning ? "New game" : "Roll"}</button>
         </main>
     )
 }
